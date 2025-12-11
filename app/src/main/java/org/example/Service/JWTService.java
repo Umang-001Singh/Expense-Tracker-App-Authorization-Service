15a3284d12eda6b785b98a5e2fc6e1f456f39d38
Umang <umang.010@singh@gmail.com>
2025-12-12 01:09:23 +0530

    Login fix.

--- app/src/main/java/org/example/Service/JWTService.java
+++ app/src/main/java/org/example/Service/JWTService.java
@@ -6,6 +6,7 @@ import io.jsonwebtoken.security.Keys;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import org.example.Repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
@@ -18,10 +19,10 @@ import java.util.function.Function;
 
 
 @Service
-@AllArgsConstructor
 @Data
 public class JWTService {
 
+    @Autowired
     private UserRepository userRepository;
 
     @Value("${jwt.secret}")
