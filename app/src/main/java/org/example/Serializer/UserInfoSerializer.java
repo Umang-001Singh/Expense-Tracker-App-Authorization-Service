709ede9741602285b3b44a096e2250b59e517a2d
Umang <umang.010@singh@gmail.com>
2025-12-20 16:45:18 +0530

    Event producer fixes.

--- app/src/main/java/org/example/Serializer/UserInfoSerializer.java
+++ app/src/main/java/org/example/Serializer/UserInfoSerializer.java
@@ -3,20 +3,20 @@ package org.example.Serializer;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.kafka.common.serialization.Serializer;
-import org.example.DTO.UserInfoRequestDto;
+import org.example.eventProducer.UserInfoEvent;
 
 @Slf4j
-public class UserInfoSerializer implements Serializer<UserInfoRequestDto> {
+public class UserInfoSerializer implements Serializer<UserInfoEvent> {
 
     @Override
-    public byte[] serialize(String s, UserInfoRequestDto userInfoRequestDto) {
+    public byte[] serialize(String s, UserInfoEvent userInfoEvent) {
         byte[] returnValue = null;
         ObjectMapper objectMapper = new ObjectMapper();
         try{
-            returnValue = objectMapper.writeValueAsString(userInfoRequestDto).getBytes();
+            returnValue = objectMapper.writeValueAsString(userInfoEvent).getBytes();
         }
         catch (Exception ex){
-            log.error("Error serializing UserInfoRequestDto", ex);
+            log.error("Error serializing UserInfoRequestDto ", ex);
         }
         return returnValue;
     }
